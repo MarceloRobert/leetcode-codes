34a3cee6827043faafe2ed8edd778014389744f8
Marcelo Robert Santos <4mrSantos@gmail.com>
2026-06-03 08:45:47 -0300

    3633 done

---
+++ solutions/typescript/3633 Earliest Finish Time for Land and Water Rides I.test.ts
@@ -0,0 +1,118 @@
+/**
+ 3633. Earliest Finish Time for Land and Water Rides I
+Easy
+
+You are given two categories of theme park attractions: land rides and water rides.
+    Land rides
+        landStartTime[i] – the earliest time the ith land ride can be boarded.
+        landDuration[i] – how long the ith land ride lasts.
+    Water rides
+        waterStartTime[j] – the earliest time the jth water ride can be boarded.
+        waterDuration[j] – how long the jth water ride lasts.
+
+A tourist must experience exactly one ride from each category, in either order.
+    - A ride may be started at its opening time or any later moment.
+    - If a ride is started at time t, it finishes at time t + duration.
+    - Immediately after finishing one ride the tourist may board the other (if it is already open) or wait until it opens.
+Return the earliest possible time at which the tourist can finish both rides.
+
+
+Example 1:
+    Input: landStartTime = [2,8], landDuration = [4,1], waterStartTime = [6], waterDuration = [3]
+    Output: 9
+    Explanation:​​​​​​​
+        - Plan A (land ride 0 → water ride 0):
+            Start land ride 0 at time landStartTime[0] = 2. Finish at 2 + landDuration[0] = 6.
+            Water ride 0 opens at time waterStartTime[0] = 6. Start immediately at 6, finish at 6 + waterDuration[0] = 9.
+        - Plan B (water ride 0 → land ride 1):
+            Start water ride 0 at time waterStartTime[0] = 6. Finish at 6 + waterDuration[0] = 9.
+            Land ride 1 opens at landStartTime[1] = 8. Start at time 9, finish at 9 + landDuration[1] = 10.
+        - Plan C (land ride 1 → water ride 0):
+            Start land ride 1 at time landStartTime[1] = 8. Finish at 8 + landDuration[1] = 9.
+            Water ride 0 opened at waterStartTime[0] = 6. Start at time 9, finish at 9 + waterDuration[0] = 12.
+        - Plan D (water ride 0 → land ride 0):
+            Start water ride 0 at time waterStartTime[0] = 6. Finish at 6 + waterDuration[0] = 9.
+            Land ride 0 opened at landStartTime[0] = 2. Start at time 9, finish at 9 + landDuration[0] = 13.
+        Plan A gives the earliest finish time of 9.
+
+Example 2:
+    Input: landStartTime = [5], landDuration = [3], waterStartTime = [1], waterDuration = [10]
+    Output: 14
+    Explanation:​​​​​​​
+        - Plan A (water ride 0 → land ride 0):
+            Start water ride 0 at time waterStartTime[0] = 1. Finish at 1 + waterDuration[0] = 11.
+            Land ride 0 opened at landStartTime[0] = 5. Start immediately at 11 and finish at 11 + landDuration[0] = 14.
+        - Plan B (land ride 0 → water ride 0):
+            Start land ride 0 at time landStartTime[0] = 5. Finish at 5 + landDuration[0] = 8.
+            Water ride 0 opened at waterStartTime[0] = 1. Start immediately at 8 and finish at 8 + waterDuration[0] = 18.
+        Plan A provides the earliest finish time of 14.​​​​​​​
+
+ 
+Constraints:
+    1 <= n, m <= 100
+    landStartTime.length == landDuration.length == n
+    waterStartTime.length == waterDuration.length == m
+    1 <= landStartTime[i], landDuration[i], waterStartTime[j], waterDuration[j] <= 1000
+*/
+
+function earliestFinishTime(landStartTime: number[], landDuration: number[], waterStartTime: number[], waterDuration: number[]): number {
+    // Thought process:
+    // looks like a data structure problem
+    // maybe work with intervals? either way it would depend on hwo the solution is designed
+    // we could do a "for each ride of type A, check each ride of type B and store the earliest finish time comparing between the pairs"
+    // maybe that will be too slow? O(n*m)
+
+    let earliestFinish = Infinity;
+
+    // Check land -> water rides
+    for (let i = 0; i < landStartTime.length; i++) {
+        const landFinish = landStartTime[i] + landDuration[i];
+        for (let j = 0; j < waterStartTime.length; j++) {
+            const mismatch = waterStartTime[j] - landFinish;
+            let waterFinish = 0;
+            if (mismatch > 0) {
+                waterFinish = waterStartTime[j] + waterDuration[j];
+            } else {
+                waterFinish = landFinish + waterDuration[j];
+            }
+
+            if (waterFinish < earliestFinish) {
+                earliestFinish = waterFinish;
+            }
+        }
+    }
+
+    // Check water -> land rides
+    for (let j = 0; j < waterStartTime.length; j++) {
+        const waterFinish = waterStartTime[j] + waterDuration[j];
+        for (let i = 0; i < landStartTime.length; i++) {
+            const mismatch = landStartTime[i] - waterFinish;
+            let landFinish = 0;
+            if (mismatch > 0) {
+                landFinish = landStartTime[i] + landDuration[i];
+            } else {
+                landFinish = waterFinish + landDuration[i];
+            }
+
+            if (landFinish < earliestFinish) {
+                earliestFinish = landFinish;
+            }
+        }
+    }
+
+    return earliestFinish;
+};
+
+describe('3633. Earliest Finish Time for Land and Water Rides I', () => {
+    test('Example 1', () => {
+        expect(earliestFinishTime([2,8], [4,1], [6], [3])).toBe(9);
+    });
+
+    test('Example 2', () => {
+        expect(earliestFinishTime([5], [3], [1], [10])).toBe(14);
+    });
+
+    test('case 480', () => {
+        expect(earliestFinishTime([99], [59], [99, 54], [85, 20])).toBe(158);
+    });
+});
