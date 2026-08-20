d4aa9df895a95894819249f52317a872e275b966
Marcelo Robert Santos <4mrSantos@gmail.com>
2026-08-20 09:43:35 -0300

    N2 done

---
+++ solutions/typescript/N2 Concatenation of Array.test.ts
@@ -0,0 +1,47 @@
+// Concatenation of Array
+// Easy
+
+// You are given an integer array nums of length n. Create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
+
+// Example 1:
+
+// Input: nums = [1,4,1,2]
+
+// Output: [1,4,1,2,1,4,1,2]
+
+// Example 2:
+
+// Input: nums = [22,21,20,1]
+
+// Output: [22,21,20,1,22,21,20,1]
+
+// Constraints:
+
+//     1 <= nums.length <= 1000.
+//     1 <= nums[i] <= 1000
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        return [...nums, ...nums];
+    }
+}
+
+describe("Concatenation of Array", () => {
+    test("Case 0", () => {
+        const solution = new Solution();
+        expect(solution.getConcatenation([1,4,1,2])).toEqual([1,4,1,2,1,4,1,2]);
+    });
+
+    test("Case 1", () => {
+        const solution = new Solution();
+        expect(solution.getConcatenation([22,21,20,1])).toEqual([22,21,20,1,22,21,20,1]);
+    });
+});
